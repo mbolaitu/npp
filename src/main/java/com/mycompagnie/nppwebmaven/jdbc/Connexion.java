9795026530be15816bd51ac5f66d1ea1e54b5e38
mbola <mbolaitu000485@gmail.com>
2017-02-15 15:20:58 +0300

    commit modiffication connexion

--- src/main/java/com/mycompagnie/nppwebmaven/jdbc/Connexion.java
+++ src/main/java/com/mycompagnie/nppwebmaven/jdbc/Connexion.java
@@ -1,4 +1,5 @@
 package com.mycompagnie.nppwebmaven.jdbc;
+import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -9,11 +10,10 @@ public class Connexion
 
     public static Connection getConnection() throws Exception
     {
-        if(Connexion.connex==null || (Connexion.connex!=null && Connexion.connex.isClosed()))
-        {
-            Class.forName("org.postgresql.Driver");
-            connex = DriverManager.getConnection("jdbc:postgres://qkdmwpqgytqbih:de925d71282b8f8800a7b0f5170d143ac5f0603bb5835bd9b7645948ec5c77bb@ec2-23-21-111-81.compute-1.amazonaws.com:5432/d3pkkno8gmsnla", "qkdmwpqgytqbih", "de925d71282b8f8800a7b0f5170d143ac5f0603bb5835bd9b7645948ec5c77bb");
-        }
-        return Connexion.connex;
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        String user = dbUri.getUserInfo().split(":")[0];
+        String pass = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath();
+        return DriverManager.getConnection(dbUrl,user,pass);
     }
 }
