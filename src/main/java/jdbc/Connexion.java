25a4d468d60eeeb3f8b46b425fb86ce4eee16d27
mbola <mbolaitu000485@gmail.com>
2017-02-15 13:50:54 +0300

    commit config connexion

--- src/main/java/jdbc/Connexion.java
+++ src/main/java/jdbc/Connexion.java
@@ -12,7 +12,7 @@ public class Connexion
         if(Connexion.connex==null || (Connexion.connex!=null && Connexion.connex.isClosed()))
         {
             Class.forName("org.postgresql.Driver");
-            connex = DriverManager.getConnection("jdbc:postgresql://localhost:5432/npp", "postgres", "itu");
+            connex = DriverManager.getConnection("jdbc:postgres://qkdmwpqgytqbih:de925d71282b8f8800a7b0f5170d143ac5f0603bb5835bd9b7645948ec5c77bb@ec2-23-21-111-81.compute-1.amazonaws.com:5432/d3pkkno8gmsnla", "qkdmwpqgytqbih", "de925d71282b8f8800a7b0f5170d143ac5f0603bb5835bd9b7645948ec5c77bb");
         }
         return Connexion.connex;
     }
