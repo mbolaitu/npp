09d81ab727b38a21c40b96cb309835da983cb7f2
mbola <mbolaitu000485@gmail.com>
2017-02-15 15:09:55 +0300

    commit modiffication servlet

---
+++ src/main/java/com/mycompagnie/nppwebmaven/servlet/Connexion.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompagnie.nppwebmaven.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+import com.mycompagnie.nppwebmaven.mapping.Login;
+
+/**
+ *
+ * @author mbola
+ */
+public class Connexion extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET");
+        response.addHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+        response.addHeader("Access-Control-Max-Age", "86400");
+        try  {
+            String user = request.getParameter("u");
+            String pass = request.getParameter("p");
+            Login log = Login.getLogin(user, pass);
+            if(log!=null) {
+                response.sendRedirect("accueil.jsp");
+                HttpSession session = request.getSession(true);
+                session.setAttribute("id_user", log.getId_utilisateur());
+            }
+            if(log==null) {
+                response.sendRedirect("index.html");
+            }
+        }catch(Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET");
+        response.addHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+        response.addHeader("Access-Control-Max-Age", "86400");
+        try  {
+            String user = request.getParameter("u");
+            String pass = request.getParameter("p");
+            Login log = Login.getLogin(user, pass);
+            if(log!=null) {
+                response.sendRedirect("accueil.jsp");
+                HttpSession session = request.getSession(true);
+                session.setAttribute("id_user", log.getId_utilisateur());
+            }
+            if(log==null) {
+                response.sendRedirect("index.html");
+            }
+        }catch(Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
